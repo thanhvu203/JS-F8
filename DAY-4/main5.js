eb0e6b453518a1f4d954f4ea324ceb81c1b30064
thanhvu203 <53229421+thanhvu203@users.noreply.github.com>
2023-02-14 16:59:56 +0700

    ok
    ok

---
+++ DAY-4/main5.js
@@ -0,0 +1,25 @@
+const email = `Akira Thanh`
+console.log(email);
+
+
+var myString = 'leu leu bon FA';
+// 1. length
+console.log(myString.length);
+// 2. Find index
+console.log(myString.indexOf('leu'),3);
+// 3. Cut string
+console.log(myString.slice(0,7));
+// 4 . Replace
+console.log(myString.replace(/leu/g,'Akira Thanh'));
+// 5. convert to upper Case
+console.log(myString.toLocaleUpperCase);
+// 6. convert to lower Case
+console.log(myString.toLocaleLowerCase);
+// 7. Trim
+console.log(myString.trim());
+// 8. Split
+var language = 'Javascript, PHP, Ruby';
+console.log(language.split(','));
+// 9. Get a character bt index
+const myString1 = 'dung nhin anh';
+console.log(myString1.charAt(2));
---
+++ DAY-4/main6.js
@@ -0,0 +1,10 @@
+var age = 20;
+var PI = 3.14;
+
+var result = 20 / 'ABC';
+console.log(isNAN(result));
+
+var myString = age.toString();
+console.log(myString);
+
+console.log(PI.toFixed(1));
